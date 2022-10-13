ead5d33ba98461fb74cb7f21100f98c3e88c2f23
Tien La <tiencla@outlook.com>
2022-10-13 16:50:04 -0400

    working guessing game no css

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+let msg1 = document.getElementById("message1");
+
 let answer = Math.floor(Math.random() * 100) + 1;
 let noGuess = 0;
+console.log(answer)
 
-function click() {
-
+function clickMe(){
+    let userGuess = document.getElementById("guess").value;
+    if(userGuess < 1 || userGuess > 100){
+        alert("Stupido enter a number between 1 and 100.");
+    }
+        else if(userGuess < answer){
+            msg1.textContent = "Too Low";
+        }
+        else if(userGuess > answer){
+            msg1.textContent = "Too High";
+        }
+        else if(userGuess === answer){
+            msg1.textContent = "YAY You Win!!";
+        
+        }
 }
+
+
